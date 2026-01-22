c1ebb162cbda52932dde2b7903194f4444c89b6c
PavlovskiiRomanMikhailovich <romanpavlovsk0808@gmail.com>
2026-01-22 12:00:40 +0300

    fix: number-input

--- components/number-input/number-input.js
+++ components/number-input/number-input.js
@@ -2,13 +2,17 @@ class NumberInput {
     constructor(root, options = {}) {
         this.root = root;
         this.maxValue = options.maxValue ?? 100;
-        this.minValue = options ?? 0;
-        this.lable = options ?? this.lable;
+        this.minValue = options.minValue ?? 0;
+        this.lable = options.lable ?? "Value";
     }
 
     async render() {
         const res = await fetch('./components/number-input/number-input.html');
         const html = await res.text();
         this.root.innerHTML = html;
+
+        const lableElement = this.root.querySelector('.number-input-lable');
+        lableElement.textContent = this.lable;
+
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const numberRoot = document.querySelector('number-root');
+const numberRoot = document.getElementById('number-root');
 const numberInput = new NumberInput(numberRoot, {});
 numberInput.render();
